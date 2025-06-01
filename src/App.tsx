d70d0a8134d4dc8b6e7c8bdb80d60b821c888243
chyngyzbb <c91618645@gmail.com>
2025-06-02 05:01:37 +0600

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './page/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./page/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <><Header/>
+    <>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
